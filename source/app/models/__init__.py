1bec209385351a252b0d8e0ef03b652942b1cbfa
Imran Shahid <narmi79@gmail.com>
2013-12-04 23:12:31 +1100

    Fixed a problem while trying to determine models’ app

--- source/app/models/__init__.py
+++ source/app/models/__init__.py
@@ -1,6 +1,6 @@
-from app.models.classes import Class
-from app.models.groups import Group
-from app.models.courses import Course, CourseMaterial, CoursePlan, CourseTeachers
-from app.models.students import Student
-from app.models.faculty import Teacher
-from app.models.results import Result, SubjectResult
+from source.app.models.classes import Class
+from source.app.models.groups import Group
+from source.app.models.courses import Course, CourseMaterial, CoursePlan, CourseTeachers
+from source.app.models.students import Student
+from source.app.models.faculty import Teacher
+from source.app.models.results import Result, SubjectResult
