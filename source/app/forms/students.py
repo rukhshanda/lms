0e51f384386ad66656f66c695a598969e8943671
Imran Shahid <narmi79@gmail.com>
2014-03-11 21:40:31 +1100

    import students from cdv file and display the data in a table.

--- source/app/forms/students.py
+++ source/app/forms/students.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django import forms
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
 from crispy_forms.helper import FormHelper
@@ -6,9 +6,11 @@ from crispy_forms.layout import Field, Layout, Submit, HTML
 from crispy_forms.bootstrap import FormActions
 
 from app.models.students import Student
+from app.models.classes import Class
+from app.models.groups import Group
 
 
-class StudentForm(ModelForm):
+class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = ('id', 'email', 'first_name', 'last_name', 'gender', 'roll_no', 'cclass', 'group')
@@ -40,7 +42,7 @@ class StudentForm(ModelForm):
             Field('group', css_class='input-sm'),
             FormActions(
                 Submit('save_changes', 'Save changes', css_class='btn-primary btn-sm'),
-                HTML('<input type="button" name="Cancel" value="Cancel" class="btn btn-default btn-sm" onclick="javascript:window.location='+ "'" +'/dashboard/teacher/'+ "'" +';" />')
+                HTML('<input type="button" name="Cancel" value="Cancel" class="btn btn-default btn-sm" onclick="javascript:window.location='+ "'" +'/dashboard/student/'+ "'" +';" />')
             ),
         )
 
@@ -60,3 +62,27 @@ class StudentForm(ModelForm):
             instance.user_permissions.add(permission)
 
         return instance
+
+
+class StudentImportForm(forms.Form):
+    cclass = forms.ModelChoiceField(label='Class', empty_label='Select a class', queryset=Class.objects.all(), required=True)
+    group = forms.ModelChoiceField(label='Group', empty_label='Select a group', queryset=Group.objects.all(), required=True)
+    file = forms.FileField(label='CSV data file', help_text='<small><em>Please select a valid CSV file. See <a href="/static/csv/import_student.csv">this template</a> for reference.</em></small>')
+
+    def __init__(self, *args, **kwargs):
+        super(StudentImportForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper(self)
+        self.helper.form_method = 'POST'
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-md-3 small'
+        self.helper.field_class = 'col-md-7'
+        self.helper.layout = Layout(
+            Field('cclass', css_class='input-sm'),
+            Field('group', css_class='input-sm'),
+            Field('file', css_class='input-sm'),
+            FormActions(
+                Submit('save_changes', 'Save changes', css_class='btn-primary btn-sm'),
+                HTML('<input type="button" name="Cancel" value="Cancel" class="btn btn-default btn-sm" onclick="javascript:window.location='+ "'" +'/dashboard/student/'+ "'" +';" />')
+            ),
+        )
--- source/app/views/dashboard/students.py
+++ source/app/views/dashboard/students.py
@@ -1,8 +1,11 @@
+import os, csv
+
+from lms.settings.base import MEDIA_ROOT
 from django.shortcuts import render, redirect, Http404
 from django.contrib.auth.decorators import login_required
 
 from app.models.students import Student
-from app.forms.students import StudentForm
+from app.forms.students import StudentForm, StudentImportForm
 
 
 @login_required(redirect_field_name=None)
@@ -77,6 +80,39 @@ def student_delete(request, id):
 @login_required()
 def student_import(request):
     if request.method == 'POST':
-        pass
+        form = StudentImportForm(request.POST, request.FILES)
+        data = None
+        if form.is_valid():
+            data = handle_uploaded_file(request.FILES['file'])
     else:
-        pass
+        form = StudentImportForm()
+        data = None
+
+    return render(
+        request,
+        'dashboard/student_import.html',
+        {
+            'form': form,
+            'data': data,
+        }
+    )
+
+
+def handle_uploaded_file(f):
+    filename = '%s/student/file.csv' % MEDIA_ROOT
+    if os.path.exists(filename):
+        os.remove(filename)
+
+    try:
+        with open(filename, 'wb+') as destination:
+            for chunk in f.chunks():
+                destination.write(chunk)
+        destination.close()
+
+        reader = csv.reader(f)
+        return reader
+        # for row in reader:
+        #     #do something with each row
+        # f.close()
+    except:
+        raise Exception('Invalid file format.')
